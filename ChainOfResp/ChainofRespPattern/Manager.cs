439342416eca6071ef844e69db149d93c84d3e13
Yasin Baltaoğlu <37988334+ayasinb@users.noreply.github.com>
2024-05-10 21:06:59 +0300

    .

---
+++ ChainOfResp/ChainofRespPattern/Manager.cs
@@ -0,0 +1,42 @@
+﻿using ChainOfResp.DAL;
+using ChainOfResp.Models;
+
+namespace ChainOfResp.ChainofRespPattern
+{
+    public class Manager:Employee
+    {
+        private readonly Context _context;
+
+        public Manager(Context context)
+        {
+            _context = context;
+        }
+
+        public override void ProcessRequest(CustomerViewModel customerViewModel)
+        {
+            if (customerViewModel.Amount <= 250000)
+            {
+                CustomerProcess customerProcess = new CustomerProcess();
+                customerProcess.Name = customerViewModel.Name;
+                customerProcess.Amount = customerViewModel.Amount;
+                customerProcess.EmployeeName = "Murat Yücedağ";
+                customerProcess.Description = "Talep edilen tutar şube müdürü tarafından ödendi.";
+                _context.CustomerProcess.Add(customerProcess);
+                _context.SaveChanges();
+            }
+
+            else if (NextApprover != null)
+            {
+                CustomerProcess customerProcess = new CustomerProcess();
+                customerProcess.Name = customerViewModel.Name;
+                customerProcess.Amount = customerViewModel.Amount;
+                customerProcess.EmployeeName = "Murat Yücedağ";
+                customerProcess.Description = "Talep edilen tutar şube müdür tarafından ödenemedi, işlem bölge müdürüne aktarıldı.";
+                _context.CustomerProcess.Add(customerProcess);
+                _context.SaveChanges();
+                NextApprover.ProcessRequest(customerViewModel);
+            }
+
+        }
+    }
+}
---
+++ ChainOfResp/ChainofRespPattern/ManagerAssistant.cs
@@ -0,0 +1,42 @@
+﻿using ChainOfResp.DAL;
+using ChainOfResp.Models;
+
+namespace ChainOfResp.ChainofRespPattern
+{
+    public class ManagerAssistant : Employee
+    {
+        private readonly Context _context;
+
+        public ManagerAssistant(Context context)
+        {
+            _context = context;
+        }
+
+        public override void ProcessRequest(CustomerViewModel customerViewModel)
+        {
+            if (customerViewModel.Amount <= 150000)
+            { 
+            CustomerProcess customerProcess = new CustomerProcess();
+            customerProcess.Name = customerViewModel.Name;
+            customerProcess.Amount = customerViewModel.Amount;
+            customerProcess.EmployeeName = "Baki Emre Çetin";
+            customerProcess.Description = "Talep edilen tutar şube müdür yardımcısı tarafından ödendi.";
+            _context.CustomerProcess.Add(customerProcess);
+            _context.SaveChanges();
+            }
+
+            else if(NextApprover!=null)
+            {
+                CustomerProcess customerProcess=new CustomerProcess();
+                customerProcess.Name = customerViewModel.Name;
+                customerProcess.Amount = customerViewModel.Amount;
+                customerProcess.EmployeeName= "Baki Emre Çetin";
+                customerProcess.Description = "Talep edilen tutar şube müdür yardımcısı tarafından ödenemedi, işlem şube müdürüne aktarıldı.";
+                _context.CustomerProcess.Add(customerProcess);
+                _context.SaveChanges();
+                NextApprover.ProcessRequest(customerViewModel);
+            }
+        
+        }
+    }
+}
