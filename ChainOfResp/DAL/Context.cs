34d930059e0a00c2b8661dab2039a13f5b7a4f85
Yasin Baltaoğlu <37988334+ayasinb@users.noreply.github.com>
2024-05-10 20:44:20 +0300

    Add project files.

---
+++ ChainOfResp/DAL/Context.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace ChainOfResp.DAL
+{
+    public class Context:DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server=AYASINB\\AYASINB;initial Catalog=ChainOfDb;integrated security=true");
+        }
+
+        public DbSet<CustomerProcess> CustomerProcess { get; set; }
+    }
+}
